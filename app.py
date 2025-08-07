e7da7be722bca8dc89332dabaf7b9f000aa80937
Ridho Febriyan <158288630+Ridhof93@users.noreply.github.com>
2025-08-07 17:58:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -79,25 +79,25 @@ if st.sidebar.button('🔍 Hitung Selisih Harga'):
 
                 col1, col2 = st.columns(2)
 
-                with col1:
+               with col1:
                     st.markdown(f"""
-                    **Model Series:** `{selected_model_series}`  
-                    **Varian:** `{selected_varian}`  
-                    **Lokasi 1:** {selected_lokasi1}  
-                    **Harga Lokasi 1:** 🏷️ **Rp {price1:,.0f}**
-                    """)
+                    <p style='font-size: 20px;'><strong>Model Series:</strong> {selected_model_series}</p>
+                    <p style='font-size: 20px;'><strong>Varian:</strong> {selected_varian}</p>
+                    <p style='font-size: 20px;'><strong>Lokasi 1:</strong> {selected_lokasi1}</p>
+                    <p style='font-size: 22px;'><strong>Harga Lokasi 1:</strong> 🏷️ <strong>Rp {price1:,.0f}</strong></p>
+                    """, unsafe_allow_html=True)
 
                 with col2:
-                    st.markdown(f"""
-                    **Lokasi 2:** {selected_lokasi2}  
-                    **Harga Lokasi 2:** 🏷️ **Rp {price2:,.0f}**  
-                    **Selisih Harga Absolut:** 💰 **Rp {absolute_price_difference:,.0f}**
-                    """)
+                     st.markdown(f"""
+                    <p style='font-size: 20px;'><strong>Lokasi 2:</strong> {selected_lokasi2}</p>
+                    <p style='font-size: 22px;'><strong>Harga Lokasi 2:</strong> 🏷️ <strong>Rp {price2:,.0f}</strong></p>
+                    <p style='font-size: 22px;'><strong>Selisih Harga Absolut:</strong> 💰 <strong>Rp {absolute_price_difference:,.0f}</strong></p>
+                    """, unsafe_allow_html=True)
 
-                    if percentage_price_difference != float('inf'):
-                        st.markdown(f"**Selisih Persentase:** 📈 `{percentage_price_difference:.2f}%`")
-                    else:
-                        st.markdown("**Selisih Persentase:** 🔁 *Tidak terdefinisi (Harga Lokasi 1 = 0)*")
+            if percentage_price_difference != float('inf'):
+                    st.markdown(f"<p style='font-size: 22px;'><strong>Selisih Persentase:</strong> 📈 {percentage_price_difference:.2f}%</p>", unsafe_allow_html=True)
+            else:
+                st.markdown("<p style='font-size: 22px;'><strong>Selisih Persentase:</strong> 🔁 Tidak terdefinisi (Harga Lokasi 1 = 0)</p>", unsafe_allow_html=True)
 
             else:
                 st.warning(f"⚠️ Data tidak ditemukan untuk kombinasi tersebut di {selected_lokasi1} atau {selected_lokasi2}.")
